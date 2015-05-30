9645c07e215b027df94182d266ae2618e8038837
HernanFaustino <chaccachuctaya@gmail.com>
2015-05-29 22:01:04 -0500

    change of the coment

--- hello_world.java
+++ hello_world.java
@@ -1,8 +1,8 @@
-//====================
-//Hello World
-//Hernan Faustino
-//29/05/15
-//===================
+/*
+Hello World
+Hernan Faustino
+29/05/15
+*/
 public class hello_world{
 	public static void main(String[] args) {
 		System.out.println("Hello World");		
